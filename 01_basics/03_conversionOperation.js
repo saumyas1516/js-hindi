f9747743de618864c1be28315fc30d6b592ddf9f
Saumya Singh <123150031+saumyas1516@users.noreply.github.com>
2024-01-23 19:20:41 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueinNumber = Number(score)
+
+console.log(typeof(valueinNumber))
+console.log(valueinNumber)
+
+let n = 1
+let ans =Boolean(n)
+console.log(ans)
+
+// 1-> true , 0-> false , ""  - false , "saumya"- true
