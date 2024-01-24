5e5d6618990cb894eca3e08e89117918f6e1ef6e
Saumya Singh <123150031+saumyas1516@users.noreply.github.com>
2024-01-24 05:29:07 +0000

    datatype summary

---
+++ 01_basics/05_datatype.js
@@ -0,0 +1,23 @@
+const num = 123;
+const float = 12.3;
+
+const id = Symbol("123")
+const anotherid =Symbol("123")
+
+const val = null;
+
+console.log(id === anotherid)
+
+let arr =["saumya" , "niwesh","me"]
+
+console.log(arr);
+ let myobj ={
+    1:"saumya",
+    2:"shagufa",
+    3:"harshek"
+ }
+
+ console.log(typeof(myobj));
+ console.log(typeof(arr));
+
+ console.log(typeof(val))
